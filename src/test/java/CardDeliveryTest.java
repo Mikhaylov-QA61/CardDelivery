fb692e3b79aebfe525966648496777470b7dfef7
Pers test <qa61test@gmail.com>
2023-07-23 22:31:33 +0400

    Test fix

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -2,45 +2,42 @@ import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.selector.ByText;
 import io.opentelemetry.sdk.metrics.data.Data;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withTagAndText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
+    public String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+    String planningDate = generateDate(5, "dd.MM.yyyy");
 
     @Test
     void successfulFormSend() {
-
-        // определение текущей даты с отступом в 3 дня;
-        var Data = new Date();
-        var currentDay = Data.getDate();
-        var currentMonth = Data.getMonth() + 1;
-        var newDay = currentDay + 3;
-        if (newDay > 31) {      // переход на следующий месяц с учетом 3-х дневного срока
-            newDay = currentDay-31;
-            currentMonth++;
-        }
-        if (currentMonth > 12) {
-            currentMonth = 01;
-
-        }
 
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=city] input").setValue("Самара");
         $(".menu").click();
-        form.$("[data-test-id=date] input").setValue(newDay + "." + currentMonth + ".2023");
+        form.$("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.UP), Keys.DELETE);
+        form.$("[data-test-id=date] input").setValue(planningDate);
         form.$("[data-test-id=name] input").setValue("Петров Иван");
         form.$("[data-test-id=phone] input").setValue("+79176666666");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         $(".spin_visible").shouldBe(visible);
         $("[data-test-id=notification].notification_visible .notification__title").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Успешно!"));
+        $("[data-test-id=notification].notification_visible .notification__content").shouldBe(visible).shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
 
     }
 
@@ -52,20 +49,19 @@ public class CardDeliveryTest {
         form.$("[data-test-id=city] input").setValue("Са");
         $$(".menu .menu-item__control").find(exactText("Самара")).click();
         form.$("[data-test-id=date] .icon-button").click();
-        var Data = new Date();
-        var currentDay = Data.getDate();
-        var newDay = currentDay + 7;
-        if (newDay > 31) {
-            newDay = currentDay-31;
-        $(".calendar__title[data-step=1]").click();
+        if(!generateDate(3,"MM").equals(generateDate(7,"MM"))){
+            $(".calendar__title[data-step=1]").click();
         }
-        $(byText(String.valueOf(newDay))).click();
+        String calendarDate = generateDate(5, "dd");
+//        $(withTagAndText("td.calendar__day", String.valueOf(calendarDate))).click();
+        $$(".calendar__day").find(exactText(calendarDate)).click();
         form.$("[data-test-id=name] input").setValue("Петров Иван");
         form.$("[data-test-id=phone] input").setValue("+79176666666");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
         $(".spin_visible").shouldBe(visible);
         $("[data-test-id=notification].notification_visible .notification__title").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(exactText("Успешно!"));
+        $("[data-test-id=notification].notification_visible .notification__content").shouldBe(visible).shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
 
     }
 }
